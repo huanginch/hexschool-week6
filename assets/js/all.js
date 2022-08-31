d5ec45efd8a6a56dd06cce50896fdea844477929
huanginch <huanginch329@gmail.com>
2022-08-31 16:39:30 +0800

    Update 2022-08-31T08:39:27.392Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var swiper1 = new Swiper(".pl-swiper", {
   spaceBetween: 10,
   slidesPerView: 2,
@@ -6,49 +8,45 @@ var swiper1 = new Swiper(".pl-swiper", {
   loop: true,
   autoplay: {
     delay: 700,
-    disableOnInteraction: false,
+    disableOnInteraction: false
   },
   breakpoints: {
     768: {
-      slidesPerView: 5,
+      slidesPerView: 5
     },
     1024: {
-      slidesPerView: 10,
-    },
-  },
+      slidesPerView: 10
+    }
+  }
 });
-
 var swiper2 = new Swiper(".share-swiper", {
   effect: 'fade',
   fadeEffect: {
     crossFade: true
   },
-
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper-pagination'
   },
-
   navigation: {
     nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+    prevEl: '.swiper-button-prev'
+  }
 });
-
 var swiper3 = new Swiper(".course-swiper", {
   spaceBetween: 16,
   slidesPerView: 2,
   breakpoints: {
     768: {
-      slidesPerView: 3,
+      slidesPerView: 3
     },
     1200: {
-      slidesPerView: 6,
-    },
-  },
+      slidesPerView: 6
+    }
+  }
 });
-
-const elem = document.querySelector('input[name="datepicker"]');
-const datepicker = new Datepicker(elem, {
+var elem = document.querySelector('input[name="datepicker"]');
+var datepicker = new Datepicker(elem, {
   autohide: true,
-  language: 'zh-CN',
+  language: 'zh-CN'
 });
+//# sourceMappingURL=all.js.map
